a4c49a0639d05696d20a1a47d8bce72450887862
jannik323 <46799517+jannik323@users.noreply.github.com>
2021-08-16 17:50:09 +0200

    level editor

--- script.js
+++ script.js
@@ -10,228 +10,236 @@ let timerhtml = document.getElementById("timerhtml");
 let timeshtml = document.getElementById("timeshtml");
 let pausemenu = document.getElementById("pausemenu");
 timeshtml.value = " Current Level Times:  ";
+
+let mouseX = 0;
+let mouseY = 0;
+let clicks = 0;
+let  selector = {};
+
+
 let TIMES = [];
 
 const keys = {};
 let GAMEOBJECTS = [];
-let level = 0;
+let level = 1;
 
 const LEVELS = [
 
     {
+        name:"Edit Level",
+        spawn:{x:150,y:10},
+        content:[
+
+
+        ],
+    },
+
+    {
         name:"Tutorial",
         spawn:{x:220,y:480},
-        build: function(){
-            
-            makegm(0,600-20,800,20);
+        content:[
+            {x:0,y:580,w:800,h:20,t:"platform"},
+            {x:40,y:430,w:130,h:30,t:"platform"},
 
-            makegm(40,430,130,30);
+            {x:200,y:520,w:40,h:40,t:"platform"},
+            {x:250,y:520,w:40,h:40,t:"platform"},
+            {x:300,y:500,w:40,h:40,t:"platform"},
+            {x:350,y:460,w:60,h:60,t:"platform"},
+            {x:420,y:480,w:40,h:80,t:"lava"},
+            {x:470,y:440,w:60,h:80,t:"platform"},
+            {x:200,y:420,w:40,h:40,t:"platform"},
+            {x:150,y:380,w:20,h:20,t:"goal"},
 
-            makegm(200,520,40,40);
-            makegm(250,520,40,40);
-            makegm(300,500,40,40);
-            makegm(350,460,60,60);
-            makegm(420,480,40,80,"lava");
-            makegm(470,440,60,80);
-            makegm(200,420,40,40);
-            makegm(150,380,20,20,"goal");
-
-        }
+        ],
     },
+
     {
         name:"Tricky Start",
         spawn:{x:20,y:550},
-        build: function(){
-            
-            
-            makegm(320,560,330,20,"lava");
+        content:[
+            {x:320,y:560,w:330,h:20,t:"lava"},
 
-            makegm(190,500,10,20,"lava");
-            makegm(100,500,10,20,"lava");
-            makegm(20,500,80,20);
-            makegm(200,500,100,20);
-            makegm(300,500,20,100);
-            makegm(0,500,20,100);
+            {x:190,y:500,w:10,h:20,t:"lava"},
+            {x:100,y:500,w:10,h:20,t:"lava"},
+            {x:20,y:500,w:80,h:20,t:"platform"},
+            {x:200,y:500,w:100,h:20,t:"platform"},
+            {x:300,y:500,w:20,h:100,t:"platform"},
+            {x:0,y:500,w:20,h:100,t:"platform"},
             
-            makegm(0,350,250,80);
-            makegm(400,350,20,235);
-            makegm(480,300,20,300);
+            {x:0,y:350,w:250,h:80,t:"platform"},
+            {x:400,y:350,w:20,h:235,t:"platform"},
+            {x:480,y:300,w:20,h:300,t:"platform"},
 
             
-            makegm(660,500,140,20);
-            makegm(640,500,20,100);
-            makegm(700,350,100,80);
+            {x:660,y:500,w:140,h:20,t:"platform"},
+            {x:640,y:500,w:20,h:100,t:"platform"},
+            {x:700,y:350,w:100,h:80,t:"platform"},
 
-            makegm(550,440,40,40);
-            makegm(0,200,230,20);
-            makegm(230,200,20,130);
+            {x:550,y:440,w:40,h:40,t:"platform"},
+            {x:0,y:200,w:230,h:20,t:"platform"},
+            {x:230,y:200,w:20,h:130,t:"platform"},
             
-            makegm(350,220,40,40);
+            {x:350,y:220,w:40,h:40,t:"platform"},
 
-            makegm(100,140,40,40,"goal");
+            {x:100,y:140,w:40,h:40,t:"goal"},
+            {x:0,y:580,w:800,h:20,t:"platform"},
 
-
-
-            
-            
-            makegm(0,600-20,800,20);
-
-        }
+        ],
     },
 
 
     {
         name:"Big Jump",
         spawn:{x:50,y:10},
-        build: function(){
+        content:[
             
-            makegm(120,600-40,800,40,"lava");
-            makegm(300,130,20,450,"lava");
+            {x:120,y:560,w:800,h:40,t:"lava"},
+            {x:300,y:130,w:20,h:450,t:"lava"},
             
-            makegm(82,120,98,20);
-            makegm(80,0,20,450);
-            makegm(20,520,100,80);
-            makegm(180,460,40,40);
-            makegm(120,350,40,40);
-            makegm(220,300,40,40);
-            makegm(180,200,20,40);
+            {x:82,y:120,w:98,h:20,t:"platform"},
+            {x:80,y:0,w:20,h:450,t:"platform"},
+            {x:20,y:520,w:100,h:80,t:"platform"},
+            {x:180,y:460,w:40,h:40,t:"platform"},
+            {x:120,y:350,w:40,h:40,t:"platform"},
+            {x:220,y:300,w:40,h:40,t:"platform"},
+            {x:180,y:200,w:20,h:40,t:"platform"},
 
-            
-
-            makegm(0,0,20,600,"lava");
-            
-            makegm(470,480,240,20);
-            makegm(480,380,80,20,"lava");
-            makegm(560,160,20,260);
-            makegm(520,160,90,20);
-            makegm(660,400,20,40);
-            makegm(700,300,20,40);
-            makegm(620,280,20,40);
-            makegm(740,200,20,40);
-            makegm(500,100,20,40);
-            makegm(440,40,20,20,"goal");
             
 
+            {x:0,y:0,w:20,h:600,t:"lava"},
+            
+            {x:470,y:480,w:240,h:20,t:"platform"},
+            {x:480,y:380,w:80,h:20,t:"lava"},
+            {x:560,y:160,w:20,h:260,t:"platform"},
+            {x:520,y:160,w:90,h:20,t:"platform"},
+            {x:660,y:400,w:20,h:40,t:"platform"},
+            {x:700,y:300,w:20,h:40,t:"platform"},
+            {x:620,y:280,w:20,h:40,t:"platform"},
+            {x:740,y:200,w:20,h:40,t:"platform"},
+            {x:500,y:100,w:20,h:40,t:"platform"},
+            {x:440,y:40,w:20,h:20,t:"goal"},
+            
 
 
 
 
 
-        }
+
+        ],
     },
 
     {
         name:"bouncy",
         spawn:{x:150,y:20},
-        build: function(){
-            makegm(510,160,70,40);
-            makegm(500,0,20,380,"lava");
+        content:[
+            {x:510,y:160,w:70,h:40,t:"platform"},
+            {x:500,y:0,w:20,h:380,t:"lava"},
             
             
             
-            makegm(100,100,100,20);
-            makegm(200,0,20,370,"lava");
-            makegm(40,520,40,40,"bounce");
-            makegm(180,520,40,40,"bounce");
-            makegm(340,320,60,40);
-            makegm(470,520,40,40,"bounce");
-            makegm(620,300,60,40);
-            makegm(740,240,40,40);
-            makegm(700,80,80,40);
-            makegm(740,40,20,20,"goal");
+            {x:100,y:100,w:100,h:20,t:"platform"},
+            {x:200,y:0,w:20,h:370,t:"lava"},
+            {x:40,y:520,w:40,h:40,t:"bounce"},
+            {x:180,y:520,w:40,h:40,t:"bounce"},
+            {x:340,y:320,w:60,h:40,t:"platform"},
+            {x:470,y:520,w:40,h:40,t:"bounce"},
+            {x:620,y:300,w:60,h:40,t:"platform"},
+            {x:740,y:240,w:40,h:40,t:"platform"},
+            {x:700,y:80,w:80,h:40,t:"platform"},
+            {x:740,y:40,w:20,h:20,t:"goal"},
             
             
-            makegm(780,0,20,440,"lava");
-            makegm(0,600-40,80,40,"lava");
-            makegm(180,600-40,640,40,"lava");
+            {x:780,y:0,w:20,h:440,t:"lava"},
+            {x:0,y:560,w:80,h:40,t:"lava"},
+            {x:180,y:560,w:640,h:40,t:"lava"},
             
 
 
-        }
+        ],
     },
 
     {
         name:"moving faster",
         spawn:{x:50,y:50},
-        build: function(){
+        content:[
             
-            makegm(520,0,20,160,"lava");
+            {x:520,y:0,w:20,h:160,t:"lava"},
             
-            makegm(400,100,20,100,"lava");
-            makegm(400,200,80,20,"lava");
-            makegm(460,200,20,80,"lava");
-            makegm(280,120,20,350,"lava");
-            makegm(280,120+350,180,20,"lava");
+            {x:400,y:100,w:20,h:100,t:"lava"},
+            {x:400,y:200,w:80,h:20,t:"lava"},
+            {x:460,y:200,w:20,h:80,t:"lava"},
+            {x:280,y:120,w:20,h:350,t:"lava"},
+            {x:280,y:120+350,w:180,h:20,t:"lava"},
             
-            makegm(600,200,20,80,"lava");
-            makegm(720,200,80,20,"lava");
-            makegm(780,210,20,70,"lava");
+            {x:600,y:200,w:20,h:80,t:"lava"},
+            {x:720,y:200,w:80,h:20,t:"lava"},
+            {x:780,y:210,w:20,h:70,t:"lava"},
             
-            makegm(100,200,20,40,"lava");
+            {x:100,y:200,w:20,h:40,t:"lava"},
             
-            makegm(200,380,80,20,"lava");
-            makegm(80,380,60,20,"lava");
-            makegm(100,420,60,20,"lava");
-            makegm(190,460,20,50,"lava");
-            makegm(0,460,40,20,"lava");
-            makegm(390,480,20,60,"lava");
-            makegm(405,290,20,40,"lava");
-            makegm(280+180,120+290,20,80,"lava");
-            makegm(80,400,200,20);
-            makegm(500,100,300,20);
-            makegm(0,100,310,20);
-            makegm(0,240,200,20);
+            {x:200,y:380,w:80,h:20,t:"lava"},
+            {x:80,y:380,w:60,h:20,t:"lava"},
+            {x:100,y:420,w:60,h:20,t:"lava"},
+            {x:190,y:460,w:20,h:50,t:"lava"},
+            {x:0,y:460,w:40,h:20,t:"lava"},
+            {x:390,y:480,w:20,h:60,t:"lava"},
+            {x:405,y:290,w:20,h:40,t:"lava"},
+            {x:280+180,y:120+290,w:20,h:80,t:"lava"},
+            {x:80,y:400,w:200,h:20,t:"platform"},
+            {x:500,y:100,w:300,h:20,t:"platform"},
+            {x:0,y:100,w:310,h:20,t:"platform"},
+            {x:0,y:240,w:200,h:20,t:"platform"},
             
-            makegm(780,280,20,200);
-            makegm(780,280,20,200);
+            {x:780,y:280,w:20,h:200,t:"platform"},
+            {x:780,y:280,w:20,h:200,t:"platform"},
 
-            makegm(0,480,220,20);
+            {x:0,y:480,w:220,h:20,t:"platform"},
 
-            makegm(600,440,40,40);
-            makegm(480,340,20,20,"goal");
-            makegm(280+175,120+270,80,20);
+            {x:600,y:440,w:40,h:40,t:"platform"},
+            {x:480,y:340,w:20,h:20,t:"goal"},
+            {x:280+175,y:120+270,w:80,h:20,t:"platform"},
 
 
-            makegm(400,280,400,20);
+            {x:400,y:280,w:400,h:20,t:"platform"},
 
-            makegm(0,540,800,20);
+            {x:0,y:540,w:800,h:20,t:"platform"},
 
-        }
+        ],
     },
 
     {
         name:"full force",
         spawn:{x:50,y:10},
-        build: function(){
+        content:[
             
-            makegm(100,300,600,160,"water");
-            makegm(550,460,150,120,"water");
-            makegm(100,460,250,20,"lava");
-            makegm(350,460,200,20);
-            makegm(20,100,100,20);
-            makegm(500,140,80,20);
-            makegm(80,300,20,300);
-            makegm(700,300,80,300);
-            makegm(350,580,350,20);
-            makegm(600,200,50,20);
-            makegm(230,520,20,20,"goal");
+            {x:100,y:300,w:600,h:160,t:"water"},
+            {x:550,y:460,w:150,h:120,t:"water"},
+            {x:100,y:460,w:250,h:20,t:"lava"},
+            {x:350,y:460,w:200,h:20,t:"platform"},
+            {x:20,y:100,w:100,h:20,t:"platform"},
+            {x:500,y:140,w:80,h:20,t:"platform"},
+            {x:80,y:300,w:20,h:300,t:"platform"},
+            {x:700,y:300,w:80,h:300,t:"platform"},
+            {x:350,y:580,w:350,h:20,t:"platform"},
+            {x:600,y:200,w:50,h:20,t:"platform"},
+            {x:230,y:520,w:20,h:20,t:"goal"},
 
             
 
 
-        }
+        ],
     },
 
     
     {
         name:"NULL",
         spawn:{x:150,y:10},
-        build: function(){
+        content:[
             
-            makegm(100,100,100,20);
+            {x:100,y:100,w:100,h:20,t:"platform"},
 
 
-        }
+        ],
     },
 
 
@@ -558,6 +566,26 @@ function main(currentTime){
 function update(){
 player1.update();
 GAMEOBJECTS.forEach(v=>{v.update();})
+
+if(level === 0){
+if(keys["1"]){
+    selector.t = "platform";
+}
+if(keys["2"]){
+    selector.t = "lava";
+}
+if(keys["3"]){
+    selector.t = "bounce";
+}
+if(keys["4"]){
+    selector.t = "goal";
+}
+if(keys["5"]){
+    selector.t = "delete";
+}}
+
+
+
 }
 
 function render(){
@@ -566,11 +594,13 @@ ctx.clearRect(0,0,canvas.width,canvas.height)
 player1.render();
 GAMEOBJECTS.forEach(v=>{v.render();})
 timer.display();
-
-
 ctx.strokeStyle = "black";
+
+ctx.strokeRect(selector.x,selector.y,selector.w,selector.h)
+
 ctx.strokeText("Speed : "+Math.abs(Math.round(player1.xa)), 10, 20);
 ctx.strokeText("Level : "+LEVELS[level].name, 80, 20);
+if(level === 0){ctx.strokeText("input type (1-5) : "+selector.t, 10, 40);}
 
 
 }
@@ -584,11 +614,16 @@ function makegm(x,y,w,h,type){
 
 function buildcurrentlevel(){
 
-    LEVELS[level].build();
+    // LEVELS[level].build();
+    LEVELS[level].content.forEach(v=>{
+        makegm(v.x,v.y,v.w,v.h,v.t);
+    })
     timer.stop();
     timer.start();
 
 }
+
+
 
 function nextlevel(amount = 1){
     GAMEOBJECTS = [];
@@ -596,7 +631,7 @@ function nextlevel(amount = 1){
     timeshtml.value = " Current Level Times: "
     TIMES.push(timer.time);
     TIMES.forEach((v,i)=>{
-        timeshtml.value = timeshtml.value + "\n " + " Level: "  + LEVELS[i].name + " : " + time(v);
+        timeshtml.value = timeshtml.value + "\n " + " Level: "  + LEVELS[i+1].name + " : " + time(v);
 
     })
     buildcurrentlevel();
@@ -607,7 +642,7 @@ function nextlevel(amount = 1){
 function hardreset(){
     GAMEOBJECTS = [];
     TIMES = [];
-    level = 0;
+    level = 1;
     timeshtml.value = " Current Level Times: "
     buildcurrentlevel();
     timer.reset();
@@ -672,3 +707,70 @@ addEventListener("keypress",e=>{
     }
 
 })
+
+function starteditor(){
+    level=0; 
+    nextlevel(0);
+    selector.t = "platform";
+
+    setTimeout(()=>{
+        onclick = function(e){
+        
+            let rect = canvas.getBoundingClientRect();
+            mouseX = Math.floor((e.clientX- rect.left) /10 )*10;
+            mouseY = Math.floor((e.clientY- rect.top) /10 )*10;
+            if(mouseX>=0 && mouseX<800 && mouseY>=0&&mouseY<600){
+    
+                if (selector.t === "delete"){
+    
+                    GAMEOBJECTS.forEach((v,i)=>{
+                    let collision = v.collision(mouseX,mouseY,mouseX,mouseY);
+                    if(collision.col){GAMEOBJECTS.splice(i,1)}
+                    })
+    
+                }else{
+                clicks ++;
+                switch(clicks){
+                    case 1:
+                        selector.x1 = mouseX;
+                        selector.y1 = mouseY;
+                        selector.x = selector.x1;
+                        selector.y = selector.y1;
+                        selector.w = 20;
+                        selector.h = 20;
+                        break;
+                    case 2:
+                        selector.x2 = mouseX;
+                        selector.y2 = mouseY;
+                        if(selector.x1 < selector.x2){
+                            selector.x = selector.x1; selector.w = selector.x2-selector.x1+20;  }
+                        else if(selector.x1 === selector.x2){
+                            selector.x = selector.x1; selector.w = 20; 
+                        }
+                        else{ 
+                            selector.x = selector.x2; selector.w = selector.x1-selector.x2+20;   }
+    
+                        if(selector.y1 < selector.y2){
+                            selector.y = selector.y1; selector.h = selector.y2-selector.y1+20;}
+                        else if(selector.y1 === selector.y2){
+                            selector.x = selector.x1; selector.h = 20; 
+                        }else{
+                            selector.y = selector.y2; selector.h = selector.y1-selector.y2+20;
+                            }
+                        break;
+                    case 3:
+                        makegm(selector.x,selector.y,selector.w,selector.h,selector.t)
+                        selector = {}
+                        selector.t = "platform"
+                        clicks = 0;
+                        break;
+                    }
+                }
+            }
+        
+        }  
+    },100)
+    
+
+
+}
