622d8408b48475d653654ba674e4dc30b4556818
jannik323 <46799517+jannik323@users.noreply.github.com>
2021-08-16 21:34:03 +0200

    bug fixes, new level

--- script.js
+++ script.js
@@ -35,209 +35,23 @@ const LEVELS = [
         ],
     },
 
-    {
-        name:"Tutorial",
-        spawn:{x:220,y:480},
-        content:[
-            {x:0,y:580,w:800,h:20,t:"platform"},
-            {x:40,y:430,w:130,h:30,t:"platform"},
+    {"content":[{"x":0,"y":580,"w":800,"h":20,"t":"platform"},{"x":40,"y":430,"w":130,"h":30,"t":"platform"},{"x":200,"y":520,"w":40,"h":40,"t":"platform"},{"x":250,"y":520,"w":40,"h":40,"t":"platform"},{"x":300,"y":500,"w":40,"h":40,"t":"platform"},{"x":350,"y":460,"w":60,"h":60,"t":"platform"},{"x":420,"y":480,"w":40,"h":80,"t":"lava"},{"x":470,"y":440,"w":60,"h":80,"t":"platform"},{"x":200,"y":420,"w":40,"h":40,"t":"platform"},{"x":150,"y":380,"w":20,"h":20,"t":"goal"}],"spawn":{"x":220,"y":480},"name":"Tutorial"},
 
-            {x:200,y:520,w:40,h:40,t:"platform"},
-            {x:250,y:520,w:40,h:40,t:"platform"},
-            {x:300,y:500,w:40,h:40,t:"platform"},
-            {x:350,y:460,w:60,h:60,t:"platform"},
-            {x:420,y:480,w:40,h:80,t:"lava"},
-            {x:470,y:440,w:60,h:80,t:"platform"},
-            {x:200,y:420,w:40,h:40,t:"platform"},
-            {x:150,y:380,w:20,h:20,t:"goal"},
-
-        ],
-    },
-
-    {
-        name:"Tricky Start",
-        spawn:{x:20,y:550},
-        content:[
-            {x:320,y:560,w:330,h:20,t:"lava"},
-
-            {x:190,y:500,w:10,h:20,t:"lava"},
-            {x:100,y:500,w:10,h:20,t:"lava"},
-            {x:20,y:500,w:80,h:20,t:"platform"},
-            {x:200,y:500,w:100,h:20,t:"platform"},
-            {x:300,y:500,w:20,h:100,t:"platform"},
-            {x:0,y:500,w:20,h:100,t:"platform"},
-            
-            {x:0,y:350,w:250,h:80,t:"platform"},
-            {x:400,y:350,w:20,h:235,t:"platform"},
-            {x:480,y:300,w:20,h:300,t:"platform"},
-
-            
-            {x:660,y:500,w:140,h:20,t:"platform"},
-            {x:640,y:500,w:20,h:100,t:"platform"},
-            {x:700,y:350,w:100,h:80,t:"platform"},
-
-            {x:550,y:440,w:40,h:40,t:"platform"},
-            {x:0,y:200,w:230,h:20,t:"platform"},
-            {x:230,y:200,w:20,h:130,t:"platform"},
-            
-            {x:350,y:220,w:40,h:40,t:"platform"},
-
-            {x:100,y:140,w:40,h:40,t:"goal"},
-            {x:0,y:580,w:800,h:20,t:"platform"},
-
-        ],
-    },
+    {"content":[{"x":320,"y":560,"w":330,"h":20,"t":"lava"},{"x":190,"y":500,"w":10,"h":20,"t":"lava"},{"x":100,"y":500,"w":10,"h":20,"t":"lava"},{"x":20,"y":500,"w":80,"h":20,"t":"platform"},{"x":200,"y":500,"w":100,"h":20,"t":"platform"},{"x":300,"y":500,"w":20,"h":100,"t":"platform"},{"x":0,"y":500,"w":20,"h":100,"t":"platform"},{"x":0,"y":350,"w":250,"h":80,"t":"platform"},{"x":400,"y":350,"w":20,"h":235,"t":"platform"},{"x":480,"y":300,"w":20,"h":300,"t":"platform"},{"x":660,"y":500,"w":140,"h":20,"t":"platform"},{"x":640,"y":500,"w":20,"h":100,"t":"platform"},{"x":700,"y":350,"w":100,"h":80,"t":"platform"},{"x":550,"y":440,"w":40,"h":40,"t":"platform"},{"x":0,"y":200,"w":230,"h":20,"t":"platform"},{"x":230,"y":200,"w":20,"h":130,"t":"platform"},{"x":350,"y":220,"w":40,"h":40,"t":"platform"},{"x":100,"y":140,"w":40,"h":40,"t":"goal"},{"x":0,"y":580,"w":800,"h":20,"t":"platform"}],"spawn":{"x":20,"y":550},"name":"Tricky Start"},
 
 
-    {
-        name:"Big Jump",
-        spawn:{x:50,y:10},
-        content:[
-            
-            {x:120,y:560,w:800,h:40,t:"lava"},
-            {x:300,y:130,w:20,h:450,t:"lava"},
-            
-            {x:82,y:120,w:98,h:20,t:"platform"},
-            {x:80,y:0,w:20,h:450,t:"platform"},
-            {x:20,y:520,w:100,h:80,t:"platform"},
-            {x:180,y:460,w:40,h:40,t:"platform"},
-            {x:120,y:350,w:40,h:40,t:"platform"},
-            {x:220,y:300,w:40,h:40,t:"platform"},
-            {x:180,y:200,w:20,h:40,t:"platform"},
+    {"content":[{"x":120,"y":560,"w":800,"h":40,"t":"lava"},{"x":300,"y":130,"w":20,"h":450,"t":"lava"},{"x":82,"y":120,"w":98,"h":20,"t":"platform"},{"x":80,"y":0,"w":20,"h":450,"t":"platform"},{"x":20,"y":520,"w":100,"h":80,"t":"platform"},{"x":180,"y":460,"w":40,"h":40,"t":"platform"},{"x":120,"y":350,"w":40,"h":40,"t":"platform"},{"x":220,"y":300,"w":40,"h":40,"t":"platform"},{"x":180,"y":200,"w":20,"h":40,"t":"platform"},{"x":0,"y":0,"w":20,"h":600,"t":"lava"},{"x":470,"y":480,"w":240,"h":20,"t":"platform"},{"x":480,"y":380,"w":80,"h":20,"t":"lava"},{"x":560,"y":160,"w":20,"h":260,"t":"platform"},{"x":520,"y":160,"w":90,"h":20,"t":"platform"},{"x":660,"y":400,"w":20,"h":40,"t":"platform"},{"x":700,"y":300,"w":20,"h":40,"t":"platform"},{"x":620,"y":280,"w":20,"h":40,"t":"platform"},{"x":740,"y":200,"w":20,"h":40,"t":"platform"},{"x":500,"y":100,"w":20,"h":40,"t":"platform"},{"x":440,"y":40,"w":20,"h":20,"t":"goal"}],"spawn":{"x":50,"y":10},"name":"Big Jump"},
 
-            
+    {"content":[{"x":510,"y":160,"w":70,"h":40,"t":"platform"},{"x":500,"y":0,"w":20,"h":380,"t":"lava"},{"x":100,"y":100,"w":100,"h":20,"t":"platform"},{"x":200,"y":0,"w":20,"h":370,"t":"lava"},{"x":40,"y":520,"w":40,"h":40,"t":"bounce"},{"x":180,"y":520,"w":40,"h":40,"t":"bounce"},{"x":340,"y":320,"w":60,"h":40,"t":"platform"},{"x":470,"y":520,"w":40,"h":40,"t":"bounce"},{"x":620,"y":300,"w":60,"h":40,"t":"platform"},{"x":740,"y":240,"w":40,"h":40,"t":"platform"},{"x":700,"y":80,"w":80,"h":40,"t":"platform"},{"x":740,"y":40,"w":20,"h":20,"t":"goal"},{"x":780,"y":0,"w":20,"h":440,"t":"lava"},{"x":0,"y":560,"w":80,"h":40,"t":"lava"},{"x":180,"y":560,"w":640,"h":40,"t":"lava"}],"spawn":{"x":150,"y":20},"name":"Bouncy"},
 
-            {x:0,y:0,w:20,h:600,t:"lava"},
-            
-            {x:470,y:480,w:240,h:20,t:"platform"},
-            {x:480,y:380,w:80,h:20,t:"lava"},
-            {x:560,y:160,w:20,h:260,t:"platform"},
-            {x:520,y:160,w:90,h:20,t:"platform"},
-            {x:660,y:400,w:20,h:40,t:"platform"},
-            {x:700,y:300,w:20,h:40,t:"platform"},
-            {x:620,y:280,w:20,h:40,t:"platform"},
-            {x:740,y:200,w:20,h:40,t:"platform"},
-            {x:500,y:100,w:20,h:40,t:"platform"},
-            {x:440,y:40,w:20,h:20,t:"goal"},
-            
+    {"content":[{"x":520,"y":0,"w":20,"h":160,"t":"lava"},{"x":400,"y":100,"w":20,"h":100,"t":"lava"},{"x":400,"y":200,"w":80,"h":20,"t":"lava"},{"x":460,"y":200,"w":20,"h":80,"t":"lava"},{"x":280,"y":120,"w":20,"h":350,"t":"lava"},{"x":280,"y":470,"w":180,"h":20,"t":"lava"},{"x":600,"y":200,"w":20,"h":80,"t":"lava"},{"x":720,"y":200,"w":80,"h":20,"t":"lava"},{"x":780,"y":210,"w":20,"h":70,"t":"lava"},{"x":100,"y":200,"w":20,"h":40,"t":"lava"},{"x":200,"y":380,"w":80,"h":20,"t":"lava"},{"x":80,"y":380,"w":60,"h":20,"t":"lava"},{"x":100,"y":420,"w":60,"h":20,"t":"lava"},{"x":190,"y":460,"w":20,"h":50,"t":"lava"},{"x":0,"y":460,"w":40,"h":20,"t":"lava"},{"x":390,"y":480,"w":20,"h":60,"t":"lava"},{"x":405,"y":290,"w":20,"h":40,"t":"lava"},{"x":460,"y":410,"w":20,"h":80,"t":"lava"},{"x":80,"y":400,"w":200,"h":20,"t":"platform"},{"x":500,"y":100,"w":300,"h":20,"t":"platform"},{"x":0,"y":100,"w":310,"h":20,"t":"platform"},{"x":0,"y":240,"w":200,"h":20,"t":"platform"},{"x":780,"y":280,"w":20,"h":200,"t":"platform"},{"x":780,"y":280,"w":20,"h":200,"t":"platform"},{"x":0,"y":480,"w":220,"h":20,"t":"platform"},{"x":600,"y":440,"w":40,"h":40,"t":"platform"},{"x":480,"y":340,"w":20,"h":20,"t":"goal"},{"x":455,"y":390,"w":80,"h":20,"t":"platform"},{"x":400,"y":280,"w":400,"h":20,"t":"platform"},{"x":0,"y":540,"w":800,"h":20,"t":"platform"}],"spawn":{"x":50,"y":50},"name":"Moving Faster"},
 
+    {"content":[{"x":100,"y":300,"w":600,"h":160,"t":"water"},{"x":550,"y":460,"w":150,"h":120,"t":"water"},{"x":100,"y":460,"w":250,"h":20,"t":"lava"},{"x":350,"y":460,"w":200,"h":20,"t":"platform"},{"x":20,"y":100,"w":100,"h":20,"t":"platform"},{"x":500,"y":140,"w":80,"h":20,"t":"platform"},{"x":80,"y":300,"w":20,"h":300,"t":"platform"},{"x":700,"y":300,"w":80,"h":300,"t":"platform"},{"x":350,"y":580,"w":350,"h":20,"t":"platform"},{"x":600,"y":200,"w":50,"h":20,"t":"platform"},{"x":230,"y":520,"w":20,"h":20,"t":"goal"}],"spawn":{"x":50,"y":10},"name":"Full Force"},
 
+    {"content":[{"x":0,"y":530,"w":160,"h":20,"t":"platform"},{"x":320,"y":450,"w":30,"h":150,"t":"platform"},{"x":520,"y":400,"w":40,"h":200,"t":"platform"},{"x":710,"y":370,"w":40,"h":230,"t":"platform"},{"x":0,"y":550,"w":810,"h":60,"t":"lava"},{"x":560,"y":260,"w":150,"h":30,"t":"platform"},{"x":0,"y":420,"w":40,"h":150,"t":"lava"},{"x":150,"y":260,"w":140,"h":40,"t":"platform"},{"x":770,"y":260,"w":30,"h":30,"t":"platform"},{"x":0,"y":260,"w":120,"h":40,"t":"platform"},{"x":120,"y":70,"w":30,"h":250,"t":"lava"},{"x":110,"y":60,"w":50,"h":30,"t":"platform"},{"x":190,"y":230,"w":30,"h":30,"t":"platform"},{"x":250,"y":130,"w":40,"h":40,"t":"platform"},{"x":440,"y":100,"w":50,"h":30,"t":"platform"},{"x":450,"y":50,"w":20,"h":20,"t":"goal"}],"spawn":{"x":80,"y":500},"name":"Windup"},
 
+    {"content":[{"x":290,"y":0,"w":20,"h":100,"t":"platform"},{"x":440,"y":120,"w":50,"h":120,"t":"lava"},{"x":430,"y":240,"w":50,"h":110,"t":"lava"},{"x":420,"y":350,"w":50,"h":100,"t":"lava"},{"x":420,"y":450,"w":50,"h":150,"t":"platform"},{"x":280,"y":120,"w":40,"h":130,"t":"lava"},{"x":330,"y":280,"w":30,"h":80,"t":"lava"},{"x":360,"y":470,"w":30,"h":70,"t":"lava"},{"x":290,"y":590,"w":20,"h":20,"t":"lava"},{"x":200,"y":410,"w":40,"h":190,"t":"lava"},{"x":280,"y":440,"w":20,"h":30,"t":"platform"},{"x":190,"y":400,"w":60,"h":20,"t":"platform"},{"x":160,"y":290,"w":80,"h":30,"t":"lava"},{"x":230,"y":220,"w":30,"h":100,"t":"lava"},{"x":60,"y":290,"w":80,"h":30,"t":"platform"},{"x":60,"y":330,"w":30,"h":260,"t":"lava"},{"x":150,"y":440,"w":60,"h":30,"t":"lava"},{"x":80,"y":560,"w":50,"h":30,"t":"lava"},{"x":220,"y":210,"w":50,"h":20,"t":"platform"},{"x":250,"y":530,"w":40,"h":70,"t":"platform"},{"x":310,"y":570,"w":110,"h":40,"t":"platform"},{"x":140,"y":90,"w":40,"h":140,"t":"lava"},{"x":130,"y":70,"w":60,"h":30,"t":"platform"},{"x":80,"y":200,"w":70,"h":30,"t":"lava"},{"x":0,"y":0,"w":30,"h":190,"t":"lava"},{"x":0,"y":180,"w":40,"h":30,"t":"platform"},{"x":0,"y":370,"w":70,"h":30,"t":"lava"},{"x":650,"y":0,"w":40,"h":250,"t":"platform"},{"x":650,"y":490,"w":40,"h":120,"t":"platform"},{"x":640,"y":470,"w":60,"h":30,"t":"lava"},{"x":640,"y":240,"w":60,"h":30,"t":"lava"},{"x":760,"y":390,"w":30,"h":210,"t":"platform"},{"x":740,"y":360,"w":60,"h":30,"t":"lava"},{"x":540,"y":540,"w":30,"h":60,"t":"platform"},{"x":520,"y":520,"w":70,"h":30,"t":"platform"},{"x":470,"y":450,"w":40,"h":30,"t":"platform"},{"x":530,"y":360,"w":30,"h":30,"t":"platform"},{"x":590,"y":300,"w":30,"h":30,"t":"platform"},{"x":500,"y":210,"w":30,"h":30,"t":"platform"},{"x":600,"y":150,"w":30,"h":30,"t":"platform"},{"x":370,"y":80,"w":160,"h":20,"t":"platform"},{"x":460,"y":0,"w":20,"h":100,"t":"platform"},{"x":500,"y":40,"w":20,"h":20,"t":"goal"}],"spawn":{"x":410,"y":40},"name":"Falling Pain"},
 
-
-
-        ],
-    },
-
-    {
-        name:"bouncy",
-        spawn:{x:150,y:20},
-        content:[
-            {x:510,y:160,w:70,h:40,t:"platform"},
-            {x:500,y:0,w:20,h:380,t:"lava"},
-            
-            
-            
-            {x:100,y:100,w:100,h:20,t:"platform"},
-            {x:200,y:0,w:20,h:370,t:"lava"},
-            {x:40,y:520,w:40,h:40,t:"bounce"},
-            {x:180,y:520,w:40,h:40,t:"bounce"},
-            {x:340,y:320,w:60,h:40,t:"platform"},
-            {x:470,y:520,w:40,h:40,t:"bounce"},
-            {x:620,y:300,w:60,h:40,t:"platform"},
-            {x:740,y:240,w:40,h:40,t:"platform"},
-            {x:700,y:80,w:80,h:40,t:"platform"},
-            {x:740,y:40,w:20,h:20,t:"goal"},
-            
-            
-            {x:780,y:0,w:20,h:440,t:"lava"},
-            {x:0,y:560,w:80,h:40,t:"lava"},
-            {x:180,y:560,w:640,h:40,t:"lava"},
-            
-
-
-        ],
-    },
-
-    {
-        name:"moving faster",
-        spawn:{x:50,y:50},
-        content:[
-            
-            {x:520,y:0,w:20,h:160,t:"lava"},
-            
-            {x:400,y:100,w:20,h:100,t:"lava"},
-            {x:400,y:200,w:80,h:20,t:"lava"},
-            {x:460,y:200,w:20,h:80,t:"lava"},
-            {x:280,y:120,w:20,h:350,t:"lava"},
-            {x:280,y:120+350,w:180,h:20,t:"lava"},
-            
-            {x:600,y:200,w:20,h:80,t:"lava"},
-            {x:720,y:200,w:80,h:20,t:"lava"},
-            {x:780,y:210,w:20,h:70,t:"lava"},
-            
-            {x:100,y:200,w:20,h:40,t:"lava"},
-            
-            {x:200,y:380,w:80,h:20,t:"lava"},
-            {x:80,y:380,w:60,h:20,t:"lava"},
-            {x:100,y:420,w:60,h:20,t:"lava"},
-            {x:190,y:460,w:20,h:50,t:"lava"},
-            {x:0,y:460,w:40,h:20,t:"lava"},
-            {x:390,y:480,w:20,h:60,t:"lava"},
-            {x:405,y:290,w:20,h:40,t:"lava"},
-            {x:280+180,y:120+290,w:20,h:80,t:"lava"},
-            {x:80,y:400,w:200,h:20,t:"platform"},
-            {x:500,y:100,w:300,h:20,t:"platform"},
-            {x:0,y:100,w:310,h:20,t:"platform"},
-            {x:0,y:240,w:200,h:20,t:"platform"},
-            
-            {x:780,y:280,w:20,h:200,t:"platform"},
-            {x:780,y:280,w:20,h:200,t:"platform"},
-
-            {x:0,y:480,w:220,h:20,t:"platform"},
-
-            {x:600,y:440,w:40,h:40,t:"platform"},
-            {x:480,y:340,w:20,h:20,t:"goal"},
-            {x:280+175,y:120+270,w:80,h:20,t:"platform"},
-
-
-            {x:400,y:280,w:400,h:20,t:"platform"},
-
-            {x:0,y:540,w:800,h:20,t:"platform"},
-
-        ],
-    },
-
-    {
-        name:"full force",
-        spawn:{x:50,y:10},
-        content:[
-            
-            {x:100,y:300,w:600,h:160,t:"water"},
-            {x:550,y:460,w:150,h:120,t:"water"},
-            {x:100,y:460,w:250,h:20,t:"lava"},
-            {x:350,y:460,w:200,h:20,t:"platform"},
-            {x:20,y:100,w:100,h:20,t:"platform"},
-            {x:500,y:140,w:80,h:20,t:"platform"},
-            {x:80,y:300,w:20,h:300,t:"platform"},
-            {x:700,y:300,w:80,h:300,t:"platform"},
-            {x:350,y:580,w:350,h:20,t:"platform"},
-            {x:600,y:200,w:50,h:20,t:"platform"},
-            {x:230,y:520,w:20,h:20,t:"goal"},
-
-            
-
-
-        ],
-    },
-
-    {
-        name:"windup",
-        spawn:{x:80,y:500},
-        content:[{"x":0,"y":530,"w":160,"h":20,"t":"platform"},{"x":320,"y":450,"w":30,"h":150,"t":"platform"},{"x":520,"y":400,"w":40,"h":200,"t":"platform"},{"x":710,"y":370,"w":40,"h":230,"t":"platform"},{"x":0,"y":550,"w":810,"h":60,"t":"lava"},{"x":560,"y":260,"w":150,"h":30,"t":"platform"},{"x":0,"y":420,"w":40,"h":150,"t":"lava"},{"x":150,"y":260,"w":140,"h":40,"t":"platform"},{"x":770,"y":260,"w":30,"h":30,"t":"platform"},{"x":0,"y":260,"w":120,"h":40,"t":"platform"},{"x":120,"y":70,"w":30,"h":250,"t":"lava"},{"x":110,"y":60,"w":50,"h":30,"t":"platform"},{"x":190,"y":230,"w":30,"h":30,"t":"platform"},{"x":250,"y":130,"w":40,"h":40,"t":"platform"},{"x":440,"y":100,"w":50,"h":30,"t":"platform"},{"x":450,"y":50,"w":20,"h":20,"t":"goal"}]
-    },
-
-    
     {
         name:"NULL",
         spawn:{x:150,y:10},
@@ -616,7 +430,7 @@ ctx.strokeRect(selector.x,selector.y,selector.w,selector.h)
 ctx.strokeText("Speed : "+Math.abs(Math.round(player1.xa)), 10, 20);
 ctx.strokeText("Level : "+LEVELS[level].name, 80, 20);
 if(level === 0){
-    ctx.strokeText("input type (1-5) : "+selector.t, 10, 40);
+    ctx.strokeText("input type (1-6) : "+selector.t, 10, 40);
     ctx.strokeStyle = "green";
     ctx.strokeText("Spawn",spawnpoint.x,spawnpoint.y+10);
 }
@@ -633,7 +447,8 @@ function makegm(x,y,w,h,type){
 
 function buildcurrentlevel(){
 
-    // LEVELS[level].build();
+    spawnpoint.x = LEVELS[level].spawn.x;
+    spawnpoint.y = LEVELS[level].spawn.y;
     LEVELS[level].content.forEach(v=>{
         makegm(v.x,v.y,v.w,v.h,v.t);
     })
@@ -794,6 +609,9 @@ function clicking(e){
         }else if(selector.t === "spawnpoint"){
             spawnpoint.x = mouseX;
             spawnpoint.y = mouseY;
+            
+            LEVELS[level].spawn.x = spawnpoint.x ;
+            LEVELS[level].spawn.y = spawnpoint.y ;   
 
         }
         else{
@@ -816,12 +634,14 @@ function clicking(e){
                     selector.x = selector.x1; selector.w = 20; 
                 }
                 else{ 
-                    selector.x = selector.x2; selector.w = selector.x1-selector.x2+20;   }
+                    selector.x = selector.x2; selector.w = selector.x1-selector.x2+20;   
+                }
+
 
                 if(selector.y1 < selector.y2){
                     selector.y = selector.y1; selector.h = selector.y2-selector.y1+20;}
                 else if(selector.y1 === selector.y2){
-                    selector.x = selector.x1; selector.h = 20; 
+                    selector.y = selector.y1; selector.h = 20; 
                 }else{
                     selector.y = selector.y2; selector.h = selector.y1-selector.y2+20;
                     }
